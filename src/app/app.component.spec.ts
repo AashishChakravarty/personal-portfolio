0e1f3c15df89f82f7c35a57b0963e1644181bb2a
AashishChakravarty <chakravartyaashish@gmail.com>
2018-08-19 03:27:19 +0530

    project name changed & favicon changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-cv!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aashish-resume!');
   }));
 });
