ee8bedb2ed10033c643dd513f03d81756082ad42
AashishChakravarty <chakravartyaashish@gmail.com>
2018-08-18 17:30:10 +0530

    template ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
