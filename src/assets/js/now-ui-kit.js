7a00aa332bde29d5ddcadb012246409654325c55
AashishChakravarty <chakravartyaashish@gmail.com>
2018-08-21 00:20:53 +0530

    extra js file removed and css & js files location changed

--- src/assets/js/now-ui-kit.js
+++ src/assets/js/now-ui-kit.js
@@ -1,21 +1,3 @@
-/*!
-
- =========================================================
- * Now-ui-kit - v1.1.0
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/now-ui-kit
- * Copyright 2017 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
-
 var transparent = true;
 
 var transparentDemo = true;
